5bd2a3efa8a5f28c1d99ac812e0c5b6aa6269801
anoushp <34724544+anoushp@users.noreply.github.com>
2024-11-13 09:39:54 +0000

    Pending changes exported from your codespace

---
+++ streamlit_rag.py
@@ -0,0 +1,77 @@
+from huggingface_hub import login
+login(token = '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')
+import streamlit as st
+import torch
+#load pdfs
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.core.prompts.prompts import SimpleInputPrompt
+from llama_index.core import Settings
+from ctransformers import AutoModelForCausalLM
+from llama_index.llms.llama_cpp import LlamaCPP
+
+st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
+
+system_prompt="""
+You are a Q&A assistant. Your goal is to answer questions as
+accurately as possible based on the instructions and context provided.
+"""
+
+
+# Default format supportable by LLama2
+query_wrapper_prompt=SimpleInputPrompt("<|USER|>{query_str}<|ASSISTANT|>")
+
+if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about RL!",
+        }
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    docs = SimpleDirectoryReader('/workspaces/local-llm-crash-course/data/').load_data()
+    Settings.llm =  LlamaCPP(
+    # You can pass in the URL to a GGML model to download it automatically
+    model_url=model_url,
+    # optionally, you can set the path to a pre-downloaded model instead of model_url
+    model_path=None,
+    temperature=0.1,
+    max_new_tokens=256,
+    # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    context_window=3900,
+    # kwargs to pass to __call__()
+    generate_kwargs={},
+    # kwargs to pass to __init__()
+    # set to at least 1 to use GPU
+    model_kwargs={"n_gpu_layers": 1},
+    verbose=True )
+    index = VectorStoreIndex.from_documents(docs)
+    return index
+
+
+index = load_data()
+
+if "query_engine" not in st.session_state:
+        st.session_state.query_engine = load_index()
+
+if prompt := st.chat_input():
+  st.session_state.messages.append({"role": "user", "content": prompt})
+  with st.chat_message("user"):
+    st.markdown(prompt)
+
+for message in st.session_state.messages:  # Write message history to UI
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        response_stream = st.session_state.query_engine.query(prompt)
+        st.write_stream(response_stream.response_gen)
+        message = {"role": "assistant", "content": response_stream.response}
+        # Add response to message history
+        st.session_state.messages.append(message)
